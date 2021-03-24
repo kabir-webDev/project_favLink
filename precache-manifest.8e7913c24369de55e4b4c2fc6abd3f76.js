ed590e2960a13f8c29dcc129f5a3751dd12967d0
kabir-webDev <kabir.webdev@gmail.com>
2021-03-25 03:11:07 +0600

    Updates

---
+++ precache-manifest.8e7913c24369de55e4b4c2fc6abd3f76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fb8126ced0b59756ac736818ccdd9a3",
+    "url": "/project_favLink/index.html"
+  },
+  {
+    "revision": "39688bdc074d9a9f061e",
+    "url": "/project_favLink/static/css/main.af704416.chunk.css"
+  },
+  {
+    "revision": "10635d365b91d5e6e558",
+    "url": "/project_favLink/static/js/2.f85aa29c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project_favLink/static/js/2.f85aa29c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39688bdc074d9a9f061e",
+    "url": "/project_favLink/static/js/main.4707879d.chunk.js"
+  },
+  {
+    "revision": "21d5474d07c6a879c2f5",
+    "url": "/project_favLink/static/js/runtime-main.3773f540.js"
+  }
+]);
