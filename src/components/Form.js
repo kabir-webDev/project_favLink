70a2e7c9faff6eaea548ac312934db35fbbf1287
kabir-webDev <kabir.webdev@gmail.com>
2021-03-25 02:59:26 +0600

    Complete Project

---
+++ src/components/Form.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      URL: "",
+      list: [],
+    };
+  }
+
+  handleChange = (input) => {
+    this.setState({ ...this.state, [input.name]: input.value });
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    let name = this.state.name;
+    let url = this.state.URL;
+    let listArray = this.state.list;
+    listArray.push({ name: name, URL: url });
+    this.setState({ ...this.state, list: listArray });
+    this.setState({ name: "" });
+    this.setState({ URL: "" });
+    this.props.handleSubmit(this.state.list);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={(e) => this.handleChange(e.target)}
+        />
+        <br />
+        <label>URL:</label>
+        <input
+          type="text"
+          name="URL"
+          value={this.state.URL}
+          onChange={(e) => this.handleChange(e.target)}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+export default Form;
---
+++ src/components/LinkContainer.js
@@ -0,0 +1,58 @@
+import React from "react";
+import Table from "./Table";
+import Form from "./Form";
+
+class LinkContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favLinks: [],
+    };
+    /* TODO - Create state object for storing favLinks */
+  }
+
+  removeCharacter = (index) => {
+    console.log("Boom!! Deleted....");
+    console.log(index);
+
+    if (index > -1) {
+      this.state.favLinks.splice(index, 1);
+      this.setState({
+        favLinks: [...this.state.favLinks],
+      });
+    }
+  };
+
+  handleSubmit = (favLink) => {
+    favLink.map((link) =>
+      this.setState({
+        favLinks: [...this.state.favLinks, link],
+      })
+    );
+
+    console.log(favLink);
+    console.log(this.state.favLinks);
+  };
+
+  render() {
+    console.log(this.state.favLinks);
+    return (
+      <div className="container">
+        <h1>My Favorite Links</h1>
+        <p>Add a new url with a name and link to the table.</p>
+
+        <Table
+          linkData={this.state.favLinks}
+          removeLink={this.removeCharacter}
+        />
+        <br />
+
+        <h3>Add New</h3>
+
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+
+export default LinkContainer;
